c2b79a3cee6a1ff7c0930f8274794c81d0534628
Mavlon4658 <mavlonbe4658>
2025-04-01 16:47:10 +0500

    project is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,39 @@ const homeSwp = new Swiper('.home .swiper', {
   watchSlidesProgress: false,
   allowTouchMove: false,
 })
+
+const calculations = document.querySelectorAll('.calculation');
+
+if (calculations.length) {
+  calculations.forEach(el => {
+    const minus = el.querySelector('.minus');
+    const plus = el.querySelector('.plus');
+    const price = el.querySelector('.calculation-value');
+
+    const priceNum = () => {
+      return parseInt(price.textContent);
+    }
+
+    const checkPriceNum = () => {
+      if (priceNum() == 0) {
+        minus.classList.add('disabled');
+      } else {
+        minus.classList.remove('disabled');
+      }
+    }
+
+    checkPriceNum();
+    
+    plus.onclick = () => {
+      price.textContent = priceNum() + 1;
+      checkPriceNum();
+    }
+
+    minus.onclick = () => {
+      if (priceNum() > 0) {
+        price.textContent = priceNum() - 1;
+        checkPriceNum();
+      }
+    }
+  })
+}
